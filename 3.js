0733686cabcecc48f6f0c60f9fe83f51a8d1465f
AlexanderR-prog <75542738+AlexanderR-prog@users.noreply.github.com>
2022-01-06 14:48:05 +0300

    Java-basics
    Java-basics

---
+++ 3.js
@@ -0,0 +1,65 @@
+/*Задание 1*/
+
+let pasword = '1234-';
+
+if (password.length >= 4) {
+    console.log('Пароль надёжный');
+}
+if (password === - ) {
+    console.log('Пароль надёжный');
+}
+if (password === _) {
+    console.log('Пароль надёжный');
+}
+else (password.length < 4) {
+    console.log('Пароль недостаточно надёжный');
+}
+else (password !== - ) {
+    console.log('Пароль недостаточно надёжный');
+}
+else (password !== _ ) {
+    console.log('Пароль недостаточно надёжный');
+}
+
+
+let pasword = '4321_';
+if (password.length >= 4 && password === - && password === _) {
+    console.log('Пароль надёжный');
+} 
+else (password.length < 4 && password !== - && password !== _) {
+    console.log('Пароль недостаточно надёжный');
+}
+
+let pasword = 'qaz-xsw';
+if (password.length >= 4 && password === - && password === _) {
+    console.log('Пароль надёжный');
+} else (password.length < 4 && password !== - && password !== _) {
+    console.log('Пароль недостаточно надёжный');
+}
+
+let pasword = '_zxd';
+if (password.length >= 4 && password === - && password === _) {
+        console.log('Пароль надёжный');
+} else (password.length < 4 && password !== - && password !== _) {
+        console.log('Пароль недостаточно надёжный');
+}
+
+/*Задание 2*/
+
+let name = "alexAndeR";
+let surname = "radKo";
+name = str.substr(0, 1);
+name = str.substr(1);
+surname = str.substr(0, 1);
+surname = str.substr(1);
+
+console.log(name.toUpperCase(str.substr(0, 1)));
+console.log(name.toLowerCase(str.substr(1)));
+console.log(surname.toUpperCase(str.substr(0, 1)));
+console.log(surname.toLowerCase(str.substr(1)));
+
+name = Alexander ? "Имя осталось без изменений" : "Имя было преобразовано";
+surname = Radko ? "Имя осталось без изменений" : "Имя было преобразовано";
+
+
+
